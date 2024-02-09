775c019793ac8aac8bf62fc8e97c991e51407e42
Marcin <xcinek1337@wp.pl>
2024-02-09 14:48:40 +0100

    added layout for static pages

---
+++ src/app/(static-pages)/policy/page.tsx
@@ -0,0 +1,8 @@
+export default function Policy() {
+	return (
+		<>
+			<h1 className="text-4xl font-bold">Polityka Prywatności</h1>
+			<p className="text-xl">Lorem ipsum dolor sit.</p>
+		</>
+	);
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,9 +46,9 @@ const products: ProductItemType[] = [
 
 export default function Home() {
 	return (
-		<section className="mx-auto max-w-md p-12 sm:max-w-2xl sm:py-16 md:max-w-4xl lg:max-w-7xl">
+		<>
 			<h3 className=" ml-2 py-4 text-2xl ">Products</h3>
 			<ProductList products={products} />
-		</section>
+		</>
 	);
 }
