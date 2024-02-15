7c1a744e33256d1d276b25e227380e3045538662
Marcin <xcinek1337@wp.pl>
2024-02-15 20:33:58 +0100

    merge

--- src/app/products/[[...pageNumber]]/layout.tsx
+++ src/app/products/[[...pageNumber]]/layout.tsx
@@ -1,5 +1,9 @@
 import { getProductsAllOrPaginated } from "@/api/products";
+<<<<<<< HEAD
 import { Pagination } from "@/ui/molecules/Pagination";
+=======
+import { Pagination } from "@/ui/organisms/Pagination";
+>>>>>>> e2e0b7eb27d26c834cd34455328602ba0d499ba8
 
 export default async function ProductsLayout({
 	children,
