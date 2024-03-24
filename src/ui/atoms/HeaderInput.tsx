17cfa1dc37b287086e4b901fc21e8a3df3b85f27
Marcin <xcinek1337@wp.pl>
2024-03-24 19:21:15 +0100

    search delayed fixed

--- src/ui/atoms/HeaderInput.tsx
+++ src/ui/atoms/HeaderInput.tsx
@@ -1,32 +1,21 @@
 "use client";
 
-import { SearchIcon } from "lucide-react";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-import Link from "next/link";
-import { type Route } from "next";
 import { useDebounce } from "@/utils/useDebounce";
 
 export const HeaderInput = () => {
 	const router = useRouter();
 	const searchParams = useSearchParams();
 	const urlQueryParamValue = searchParams.get("query")?.toString();
-	const [searchValue, setSearchValue] = useState<string>(urlQueryParamValue || "");
+	const [searchValue, setSearchValue] = useState<string>(urlQueryParamValue ?? "");
+	const debouncedSearch = useDebounce(searchValue);
 
-	const createQueryString = useCallback(
-		(name: string, value: string) => {
-			const params = new URLSearchParams(searchParams.toString());
-			params.set(name, value);
-
-			return params.toString();
-		},
-		[searchParams],
-	);
-
-	const debouncedSearch: string = useDebounce(searchValue, 1500);
-
-	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+	const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setSearchValue(event.target.value);
+		if (event.target.value.length === 0) {
+			router.push("/products");
+		}
 	};
 
 	useEffect(() => {
@@ -37,12 +26,6 @@ export const HeaderInput = () => {
 
 	return (
 		<div className="flex items-center justify-center gap-2 rounded-md border bg-white">
-			<Link
-				href={(`/search` + "?" + createQueryString("query", searchValue)) as Route}
-				className="flex h-full w-[40px] cursor-pointer items-center justify-center border-r bg-white"
-			>
-				<SearchIcon color="gray" size={16} />
-			</Link>
 			<input
 				className="w-42 rounded-md p-1 text-sm outline-none"
 				type="search"
