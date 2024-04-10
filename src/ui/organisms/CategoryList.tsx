0ed9fa0f0e8a1df7f209fa16055e66e6ca866396
Marcin <xcinek1337@wp.pl>
2024-04-10 23:12:09 +0200

    collections card source href fixed

--- src/ui/organisms/CategoryList.tsx
+++ src/ui/organisms/CategoryList.tsx
@@ -10,6 +10,7 @@ export const CategoryList = ({ categories }: ProductListProps): JSX.Element => (
 	<ul className="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-3">
 		{categories.map((category) => (
 			<CardLink
+				src={`${category.name}.avif`}
 				name={category.name}
 				key={category.id}
 				href={`/categories/${category.slug}/1` as Route}
